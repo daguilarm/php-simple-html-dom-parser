cf37dca1d0ed38d063125600e8029a7074e26d29
daguilarm <damian.aguilarm@gmail.com>
2019-01-22 19:26:03 +0100

    updated to php 7.1

---
+++ Src/Daguilar/PhpSimple/HtmlDomParser.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Daguilarm\PhpSimple;
+
+require 'SimpleHtmlDom'.DIRECTORY_SEPARATOR.'simple_html_dom.php';
+
+class HtmlDomParser {
+
+	/**
+	 * @return \SimpleHtmlDom\simple_html_dom
+	 */
+	static public function file_get_html() {
+		return call_user_func_array ( '\SimpleHtmlDom\file_get_html' , func_get_args() );
+	}
+
+	/**
+	 * get html dom from string
+	 * @return \SimpleHtmlDom\simple_html_dom
+	 */
+	static public function str_get_html() {
+		return call_user_func_array ( '\SimpleHtmlDom\str_get_html' , func_get_args() );
+	}
+}
